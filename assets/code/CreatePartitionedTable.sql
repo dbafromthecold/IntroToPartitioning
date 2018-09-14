daeea7cf769046324cf9f8f3530b6c5990becc6f
dbafromthecold <dbafromthecold@gmail.com>
2018-09-14 14:19:57 +0100

    removed slides from branch

--- assets/code/CreatePartitionedTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE dbo.PartitionedTable
-	(ColA INT IDENTITY(1,1),
-	 ColB VARCHAR(10),
-	 ColC VARCHAR(10),
-	 ColD VARCHAR(10),
-	 PartitioningKey DATE)
-ON PartitionScheme(PartitioningKey);
